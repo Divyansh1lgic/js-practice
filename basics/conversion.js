f6e90944be79b923c555bc6893b2c9c274a12a99
Divyansh1lgic <147187700+Divyansh1lgic@users.noreply.github.com>
2024-07-21 18:48:03 +0000

    coversion

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+let score = true;
+
+console.log(typeof score);
+
+let valueInNumeber = Number(score);
+console.log(typeof valueInNumeber);
+console.log(valueInNumeber);
+
+// "33"=> 33
+// "33abs" => Nan =>not a number
+
+let isLoggedin = 1;
+let booleanIsLoggedIn = Boolean(isLoggedin);
+console.log(booleanIsLoggedIn);
