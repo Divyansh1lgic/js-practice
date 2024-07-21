bef1022b849dabafff44a3697522dd3c8fb1688c
Divyansh1lgic <147187700+Divyansh1lgic@users.noreply.github.com>
2024-07-21 18:19:45 +0000

    submission2

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,11 @@
 console.log("hitesh");
+
+const accountId = 144553  //cannot change value its constant
+let accountEmail ="divyanshbjp7@gmail.com"  //these values can be changed
+var accountPassword ="9198427673"  //dont use var because of issue in block space and functional scope
+accountCity = "jaipur"  // we can also declare like this but its not preffered
+let accountState;//if no value assigned then its undefined
+
+console.log(accountId);
+
+console.table([accountId ,accountEmail , accountPassword , accountCity , accountState]);
